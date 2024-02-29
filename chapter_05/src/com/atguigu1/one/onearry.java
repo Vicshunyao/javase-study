99d9760c005b1827b3b4fdc23ae9fa0713c3ae70
kingsy <2716255013@qq.com>
2024-02-29 15:54:23 +0800

    初始化

---
+++ chapter_05/src/com/atguigu1/one/onearry.java
@@ -0,0 +1,18 @@
+package com.atguigu1.one;
+
+public class onearry {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2;
+        num2=20;
+        //todo 数组初始化
+        double[] prices;
+        prices=new double[]{20.32,43.21,43.22};//静态
+        String []food;
+        food=new String[4];//动态
+
+
+
+
+    }
+}
---
+++ src/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello,world000");
+        //todo 定义相关数据
+    }
+}
